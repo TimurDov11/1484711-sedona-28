4acc6315a1b13d04e2661c9df55608c5108d509b
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2020-07-02 21:43:21 +0500

    Завершаем вёрстку

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 let formButton = document.querySelector('.form-button');
 let searchForm = document.querySelector('.search-form');
+let adults = searchForm.querySelector('.adults-item');
+let adultsAmount = adults.querySelector('[name=number-of-adults]');
+let children = searchForm.querySelector('.children-item');
+let childrenAmount = children.querySelector('[name=number-of-children]');
+
 
 formButton.onclick = function () {
   console.log('Кнопка нажата!');
